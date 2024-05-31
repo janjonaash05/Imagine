50d72dd65a8685d74d4703a30a5b249ad8b3d315
janjonaash05 <janjonaash05@gmail.com>
2024-05-31 12:43:26 +0200

    EnemySpawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] prefabs;
+
+    private System.Random rand;
+    [SerializeField] private float delay;
+    [SerializeField] private float upOffset;
+    private void Awake()
+    {
+        Assert.IsTrue(prefabs.Length > 0);  
+        rand = new();
+
+        InvokeRepeating(nameof(Spawn), (float) rand.NextDouble() *delay, delay);
+    }
+
+
+
+    private void Spawn() 
+    {
+       Instantiate(prefabs[rand.Next(prefabs.Length)], transform.position+ upOffset* Vector3.up, transform.rotation);
+
+
+    
+    }
+
+
+
+    
+
+
+
+
+    
+}
